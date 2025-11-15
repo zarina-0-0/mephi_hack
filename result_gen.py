964f24fae21a021dc27c4f0b219e7ca731913dcc
zarina-0-0 <b.zarina_2601@mail.ru>
2025-11-15 11:11:29 +0300

    some changes

--- result_gen.py
+++ result_gen.py
@@ -9,10 +9,6 @@ def api_get_result(text):
     )
 
     completion = client.chat.completions.create(
-      # extra_headers={
-      #   "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
-      #   "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
-      # },
       model="google/gemini-2.0-flash-exp:free",
       messages=[
         {
